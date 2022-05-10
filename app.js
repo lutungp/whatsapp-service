48faf1002419c67acdb726b4d2872d96ef90e458
lintang permana <lntngp19@gmail.com>
2022-05-10 07:26:14 +0700

    update wa service

--- app.js
+++ app.js
@@ -14,9 +14,32 @@ const PORT = process.env.PORT || 3000; //Line 3
 
 var path = require('path');
 const { url } = require('inspector');
+const { response } = require('express');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: __dirname })
+});
+
+app.post('/send-message', (req, res) => {
+    const number = req.body.number;
+    const message = req.body.message;
+
+    console.log(message)
+
+    client.sendMessage(number, message).then(response => {
+        res.status(200).json({
+            status: true,
+            response: response
+        });
+    }).catch(err => {
+        res.status(500).json({
+            status: false,
+            response: err
+        });
+    });
 });
 
 const client = new Client({ puppeteer: { headless: true }, authStrategy: new LocalAuth() });
